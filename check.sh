7736e3e21a1012efdab76425717fc4bb3fc9ef02
rjsrisri <you@example.com>
2020-09-20 22:49:55 +0530

    updated script

--- check.sh
+++ check.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-# git log --since="12 hours ago" | grep .
-git log --since="24 hours ago" --stat |grep testing
+find . -not -path '*/\.*' -type d > abc.txt
+sed -i -e '1d' abc.txt  && sed -i -e 's/^..//' abc.txt
+cat abc.txt
+
+while read line
+do
+    array+=("$line")
+done <  abc.txt
+for ((i=0; i < ${#array[*]}; i++))
+do
+git log --since="24 hours ago" --stat |grep ${array[i]}
 if [ $? == 0 ];
 then
-echo "Building image"
+echo "Building image-------Found updates in  ${array[i]} folder"
+docker build -t pivotchaindata.com:8443/${array[i]}:v01 .
+echo "Image Build --> $?"
 else
-echo "Not Found Any Updates"
+echo "Not Found Any Updates in ${array[i]}"
 fi
+done
