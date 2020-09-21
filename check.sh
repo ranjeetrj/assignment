79f18f6983ae7ea0c6458a8fb04c9e36d211479d
ranjeetrj <65105643+ranjeetrj@users.noreply.github.com>
2020-09-21 11:19:24 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+cd $WORKSPACE/Backend
 find . -maxdepth 1 -type d | cut -c 3- > abc.txt
 sed -i -e '1d' abc.txt
 cat abc.txt
@@ -13,13 +15,12 @@ git log --since="24 hours ago" --stat |grep ${array[i]}
 if [ $? == 0 ];
 then
 echo "Building image-------Found updates in  ${array[i]} folder"
-docker build -t pivotchaindata.com:8443/${array[i]}:v01 .
+docker build -t pivotchaindata.com:8443/${array[i]}:$BUILD_NUMBER .
 echo "Image Build --> $?"
-docker push pivotchaindata.com:8443/${array[i]}:v01 
+docker push pivotchaindata.com:8443/${array[i]}:$BUILD_NUMBER
 
 echo "Image Push --> $?"
 else
 echo "Not Found Any Updates in ${array[i]}"
 fi
 done
-
